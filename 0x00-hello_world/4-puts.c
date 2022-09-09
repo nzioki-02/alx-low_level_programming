501011e9ea3f3997e9b7bb6295d5f1fae214abd1
nzioki-02 <nzioki02@gmail.com>
2022-09-08 20:11:31 -0700

    Corrections on 4, 5 and 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
 int main(void)
 {
-	char string[] = "Programming is like building a multilingual puzzle";
+	char string[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(string);
 

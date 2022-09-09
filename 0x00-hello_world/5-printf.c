70eed0d9c0b86adfe9ac323b3a988291b498c99d
nzioki-02 <nzioki02@gmail.com>
2022-09-08 19:40:15 -0700

    Updating 5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/*
+ * main -Entry point
+ *
+ * Description: Using printf to display
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

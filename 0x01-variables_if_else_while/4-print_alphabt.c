6e779c67506946d3ab8ef99282f3d31129dc0881
nzioki-02 <nzioki02@gmail.com>
2022-09-09 19:37:34 -0700

    0X01. C- Variables, if, else, while.

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != e && letter != q)
+			putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry  Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i < '10'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}

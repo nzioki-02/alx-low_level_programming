57ab181568c047095e97a7befe247e0a08edc114
nzioki-02 <nzioki02@gmail.com>
2022-09-15 11:24:15 -0700

    attempting 6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs : Found in main
+ * Description: Computes the absolute value of an integer
+ * Return: absolute value
+ */
+
+int _abs(int r)
+{
+	int r;
+
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}

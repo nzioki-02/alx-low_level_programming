327d1443b1a6805c7169bb0b5c8c696580ccaf4a
nzioki-02 <nzioki02@gmail.com>
2022-09-15 17:28:19 -0700

    Debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinate loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinate loop incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	{
+		/*putchar(i);*/
+	}
+
+	printf("Infinate loop avoided! \\o/\n");
+
+	return (0);
+}

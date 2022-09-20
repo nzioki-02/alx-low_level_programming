1277d2304996a0b2305f5a8710b73059352f7111
nzioki-02 <nzioki02@gmail.com>
2022-09-19 18:04:23 -0700

    Files 0,1,2 and main.h

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes pointer to an int as parameter
+ * @n: int pointer
+ * Description: Updates pointer value to 98
+ * Return: none
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - Interchanges the value of two integers
+ * @a: int value
+ * @b: int value
+ * Return: none
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = 0;
+
+	*a = c;
+	*b = *a;
+	c = *b;
+}

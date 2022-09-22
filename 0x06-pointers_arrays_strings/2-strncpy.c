a01ae8bfa2518a8c2e4fc4c24bef9d2f5f3a45f1
nzioki-02 <nzioki02@gmail.com>
2022-09-22 16:38:29 -0700

    Corrections on 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,17 +4,19 @@
  * *_strncpy - copies a sting
  * @dest: pointer to destination of copy
  * @src: pointer to the string to be copied
- * @n: counter variable
- * Return: dest pointer value
+ * @n: no. of times to be copied
+ * Return:dest pointer value
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	for (n = 0; src[n] != '\0'; n++)
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[n] = src[n];
+		dest[i] = src[i];
 	}
-	dest[n] = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }

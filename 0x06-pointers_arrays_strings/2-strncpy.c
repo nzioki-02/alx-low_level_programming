892550ee40db1a9199d1d14ea4606edd898662e5
nzioki-02 <nzioki02@gmail.com>
2022-09-22 17:07:30 -0700

    Changes to 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,14 +12,15 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+	/*char *ptr = dest;*/
 
-	if (dest == NULL)
-		return (NULL);
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
+	for (; i < n; i++)
+	{
 	dest[i] = '\0';
-
+	}
 	return (dest);
 }

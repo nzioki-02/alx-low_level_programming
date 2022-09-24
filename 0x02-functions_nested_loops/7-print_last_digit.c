ca7acd2e9244f997634fe5d9e39dfaea407b9b05
nzioki-02 <nzioki02@gmail.com>
2022-09-23 19:03:47 -0700

    Changes to file in 0x02

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_last_digit - Found in main
+ * Description: returns the value of the last digit
+ * Return: The value of the last digit
+ */
+
+int print_last_digit(int)
+{
+	int i;
+

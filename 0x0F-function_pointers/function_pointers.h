e2e84dae2d5969067bffbd608d4f82ce75cdbc1f
nzioki-02 <nzioki02@gmail.com>
2022-10-13 17:27:39 -0700

    0x0F-function_pointers: Updating README and header file

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTIONS_POINTERS.H
+#define FUNCTIONS_POINTERS.H
+
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int (*get_op_func(char *s))(int, int);
+
+#endif
